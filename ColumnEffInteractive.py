7139355eb2716717a6468358c40f1654fa983e10
nanhechem <nanhechina@hotmail.com>
2018-03-31 16:59:32 -0500

    Calculate the GC column efficiency

---
+++ ColumnEffInteractive.py
@@ -0,0 +1,25 @@
+def column():
+        a = float(input('What is the retention time? '))
+        b = float(input('Peak width at half maximum? '))
+        c = float(input('Column length? '))
+                
+        eff = int((5.54*(a/b)**2)/c)
+        if a/c < 0.4:
+                print ("Retention time is too short. The column might be leaked")
+        else:
+                if eff > 2000:
+                        print ("Good Job, this is a good column")
+                        print (eff)
+                        
+                elif eff <2000 and eff > 1000:
+                        print ("So close. Try harder!")
+                        return eff
+                else:
+                        print ("Please keep woring! never give up")
+                        return eff
+
+			
+	
+column()
+
+#Thanks to Marcelino!
